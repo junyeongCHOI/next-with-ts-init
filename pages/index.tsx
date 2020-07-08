03dacf994eced386decefa770504b47af3752d46
junyeongCHOI <asxd153@gmail.com>
2020-07-09 07:26:15 +0900

    project init

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Index = () => {
+  return (
+    <div>
+      <Title>Test</Title>
+    </div>
+  );
+};
+
+export default Index;
+
+const Title = styled.h1`
+  color: red;
+`;
